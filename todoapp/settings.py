fe6a4ef5fb845e49e86dc5b2fe5350b64f0a4f5c
Michael Hilton <michael.hilton@gmail.com>
2019-10-16 11:42:05 -0400

    move files up a level

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -78,8 +78,14 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DBNAME'],
+        'HOST': os.environ['DBHOST'],
+        'USER': os.environ['DBUSER'],
+        'PASSWORD': os.environ['DBPASS'],
+	'OPTIONS': {
+		'sslmode':'require'
+        }
     }
 }
 
